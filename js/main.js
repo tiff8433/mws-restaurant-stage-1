e7f39d8ca92a076fbb6f8851fe55c0b990cc331c
tiff8433 <tiffany.l.huang@gmail.com>
2018-06-22 16:34:45 -0700

    Add proper DOM order

--- js/main.js
+++ js/main.js
@@ -182,7 +182,7 @@ createRestaurantHTML = (restaurant) => {
   div.append(picture);
 
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   div.append(name);
 
